dd859979bde2c4f5b2a7fb85fc87755b19fb9898
Jerry Vander Weide <jerryvanderweide72@gmail.com>
2020-01-09 12:52:11 -0500

    Started working on the Javascript for the clock app

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
- let digitalClock = function() {
-    let
+
+window.onload = function() {
+    time();
 }
+
+function time() {
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let date = (today.getMonth() + 1) + " " + today.getDate() + ", " + today.getFullYear();
+    
+    
+    time = document.getElementById("time").innerHTML = time;
+    date = document.getElementById("date").innerHTML = date;
+
+    let clockTick = function() {
+        today.getSeconds()++;
+        return setInterval(clockTick, 1000);
+    }
+
+    console.log(clockTick());
+}
+
