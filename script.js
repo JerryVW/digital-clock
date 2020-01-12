b12c7346567f9f268c98b4675d4af400b1189208
Jerry Vander Weide <59615980+JerryVW@users.noreply.github.com>
2020-01-12 12:32:02 -0500

    Corrected issue with time change.

--- script.js
+++ script.js
@@ -10,12 +10,10 @@ function clock() {
     let timeShift = hours >= 12 ? "PM" : "AM";
     const day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    
-    //let date = today.toDateString();
-    
+   
     function timer() {
         let today = new Date();
-        let t = (hours < 10 ? "0" : "") + today.getHours() % 12 + 
+        let t = (today.getHours() % 12 || 12) + 
         ":" + (today.getMinutes() < 10 ? "0" : "") + today.getMinutes() + 
         ":" + (today.getSeconds() < 10 ? "0" : "") + today.getSeconds() +
         " " + timeShift;
@@ -25,8 +23,7 @@ function clock() {
     timer();
     setInterval(timer, 1000); 
 
-    let date = day[today.getDay()] + " " + month[today.getMonth()] + " " + today.getDate() + " " + today.getFullYear();
+    let date = day[today.getDay()] + " " + month[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
     document.getElementById("date").innerHTML = date;
           
 }
-
