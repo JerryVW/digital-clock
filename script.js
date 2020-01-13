c74685ce3f61349013548ee7fbd3970a398f7335
Jerry Vander Weide <59615980+JerryVW@users.noreply.github.com>
2020-01-12 20:49:57 -0500

    Fixed issues with time change from AM to PM

--- script.js
+++ script.js
@@ -5,25 +5,26 @@ window.onload = function() {
 }
 
 function clock() {
-    let today = new Date();
-    let hours = today.getHours();
-    let timeShift = hours >= 12 ? "PM" : "AM";
-    const day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-   
     function timer() {
         let today = new Date();
-        let t = (today.getHours() % 12 || 12) + 
-        ":" + (today.getMinutes() < 10 ? "0" : "") + today.getMinutes() + 
-        ":" + (today.getSeconds() < 10 ? "0" : "") + today.getSeconds() +
-        " " + timeShift;
-        document.getElementById("time").innerHTML = t;
-        console.log("Timer is running!");
+        let hours = today.getHours();
+        let minutes = today.getMinutes();
+        let seconds = today.getSeconds();
+        let timeShift = hours >= 12 ?  "PM" : "AM";
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        let time = hours + ":" + minutes + ":" + seconds + " " + timeShift; 
+        document.getElementById("time").innerHTML = time;
     }
     timer();
     setInterval(timer, 1000); 
 
+    let today = new Date();
+    const day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
     let date = day[today.getDay()] + " " + month[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
-    document.getElementById("date").innerHTML = date;
-          
+    document.getElementById("date").innerHTML = date; 
 }
